cea209cdaee4ab7f7fae5d5e3daf369c60a3d89e
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2026-03-30 12:37:47 +0200

    Remove obsolete users management and related files

--- backend/django_services/users/throttles.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.throttling import AnonRateThrottle
-
-
-class LoginRateThrottle(AnonRateThrottle):
-    scope = "login"
-
-
-class PasswordResetRateThrottle(AnonRateThrottle):
-    scope = "password_reset"
